b72769b4ce80374e13bf9b5e50bea34619fafe99
K21005255 <K21005255@kcl.ac.uk>
2022-03-11 14:53:16 +0000

    Stats window general format

--- StatisticsViewer.java
+++ StatisticsViewer.java
@@ -7,6 +7,7 @@ import javafx.scene.control.Button;
 import javafx.scene.control.Label;
 import javafx.scene.layout.GridPane;
 import javafx.stage.Stage;
+import javafx.scene.layout.VBox;
 
 /**
  * Write a description of class StatisticsViewer here.
@@ -22,9 +23,58 @@ public class StatisticsViewer extends Stage
     /**
      * Constructor for objects of class StatisticsViewer
      */
-    public StatisticsViewer()
+    public void start(Stage stage)
     {
-        // initialise instance variables
-        x = 0;
+        // The layout of the window
+        VBox window = new VBox();
+        GridPane statsGrid = new GridPane(); 
+        VBox reviews = new VBox();
+        VBox available = new VBox(); 
+        VBox noHomeAndAppartments = new VBox();
+        VBox expensive = new VBox(); 
+          
+        
+        // The "title" labels in the window
+        Label title = new Label("Statistics");
+        Label reviewTitle = new Label("Average Reviews Per Property:");
+        Label availableTitle = new Label("Total Available Properties:");
+        Label noHomeAndAppartmentsTitle = new Label("Entire Home and Apartments:");
+        Label expensiveTitle = new Label("Most Expensive Borough:");
+        
+        // The information labels in the window
+        Label reviewInfo = new Label("default");
+        Label availableInfo = new Label("default");
+        Label noHomeAndAppartmentsInfo = new Label("default");
+        Label expensiveInfo = new Label("default");
+        
+        // Adding components 
+        window.getChildren().add(title); 
+        window.getChildren().add(statsGrid); 
+        
+        statsGrid.add(reviews, 0, 0);
+        statsGrid.add(available, 0, 1);
+        statsGrid.add(noHomeAndAppartments, 1, 0);
+        statsGrid.add(expensive, 1, 1);
+        
+        
+        reviews.getChildren().add(reviewTitle); 
+        reviews.getChildren().add(reviewInfo);
+        
+        available.getChildren().add(availableTitle); 
+        available.getChildren().add(availableInfo);
+        
+        noHomeAndAppartments.getChildren().add(noHomeAndAppartmentsTitle); 
+        noHomeAndAppartments.getChildren().add(noHomeAndAppartmentsInfo);
+        
+        expensive.getChildren().add(expensiveTitle); 
+        expensive.getChildren().add(expensiveInfo);
+        
+        Scene scene = new Scene(window, 300,100);
+        stage.setTitle("Test");
+        stage.setScene(scene);
+
+        // Show the Stage (window)
+        stage.show();
+        
     }
 }
