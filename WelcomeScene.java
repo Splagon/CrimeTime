3c3d02dbfb0149a2d5d18a26051ecb3882bf0f05
Suddens-Spiers, Charles (k21040272) <charles.suddens-spiers@kcl.ac.uk>
2022-03-11 14:13:01 +0000

    merge

--- WelcomeScene.java
+++ WelcomeScene.java
@@ -19,6 +19,7 @@ public class WelcomeScene extends Scene
 {
     // instance variables - replace the example below with your own
     private int x;
+
     /**
      * Constructor for objects of class WelcomeScene
      */
