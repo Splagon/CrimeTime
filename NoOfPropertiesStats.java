e766611fa067246f363e4cd09b9febd8ca159167
Higham, Michael (k21051343) <michael.higham@kcl.ac.uk>
2022-03-15 23:57:44 +0000

    map now properly uses quartiles

---
+++ NoOfPropertiesStats.java
@@ -0,0 +1,75 @@
+import java.util.ArrayList;
+
+public class NoOfPropertiesStats
+{
+    private ArrayList<BoroughListing> sortedNumberOfPropertiesInBorough;
+    
+    private int minPrice;
+    private int maxPrice;
+    
+    private int minNoOfPropertiesInBorough;
+    private int maxNoOfPropertiesInBorough;
+    
+    private int firstQuartile;
+    private int median;
+    private int thirdQuartile;
+    
+    private int noOfBoroughs;
+    
+    public NoOfPropertiesStats(StatisticsData dataHandler, int minPrice, int maxPrice) throws Exception {
+        this.minPrice = minPrice;
+        this.maxPrice = maxPrice;
+        sortedNumberOfPropertiesInBorough = dataHandler.getSortedNumberOfPropertiesInBoroughs(minPrice, maxPrice);
+        calculateStats();
+    }
+    
+    private void calculateStats() {
+        noOfBoroughs = sortedNumberOfPropertiesInBorough.size();
+        
+        BoroughListing boroughWithMinNoOfProperties = sortedNumberOfPropertiesInBorough.get(0);
+        //BoroughListing boroughWithMedianNoOfProperties;
+        BoroughListing boroughWithMaxNoOfProperties = sortedNumberOfPropertiesInBorough.get(sortedNumberOfPropertiesInBorough.size()-1);
+        
+        minNoOfPropertiesInBorough = boroughWithMinNoOfProperties.getNoOfPropertiesInBorough();
+        maxNoOfPropertiesInBorough = boroughWithMaxNoOfProperties.getNoOfPropertiesInBorough();
+        
+        firstQuartile = calculateQuartile(1);
+        median = calculateQuartile(2);
+        thirdQuartile = calculateQuartile(3);
+    }
+    
+    private int calculateQuartile(int quartileNumber) {
+        double quartileFraction = (double) quartileNumber / 4;
+        int quartile;
+        
+        double quartileIndex = ((double) noOfBoroughs * quartileFraction) - 1;
+        
+        if (quartileIndex - (int) quartileIndex != 0) {
+            BoroughListing boroughLowerOfQuartile = sortedNumberOfPropertiesInBorough.get((int) (noOfBoroughs * quartileFraction)-1);
+            BoroughListing boroughUpperOfQuartile = sortedNumberOfPropertiesInBorough.get((int) (noOfBoroughs * quartileFraction));
+            
+            int sizeLowerOfQuartile = boroughLowerOfQuartile.getNoOfPropertiesInBorough();
+            int sizeUpperOfQuartile = boroughUpperOfQuartile.getNoOfPropertiesInBorough();
+            
+            quartile = (int) Math.round(((sizeUpperOfQuartile - sizeLowerOfQuartile) * quartileFraction) + sizeLowerOfQuartile);
+        }
+        else {
+            BoroughListing boroughWithQuartileNoOfProperties = sortedNumberOfPropertiesInBorough.get((int) (noOfBoroughs * quartileFraction)-1);
+            quartile = boroughWithQuartileNoOfProperties.getNoOfPropertiesInBorough();
+        }
+        
+        return quartile;
+    }
+    
+    public int getMedian() {
+        return median;
+    }
+    
+    public int getFirstQuartile() {
+        return firstQuartile;
+    }
+    
+    public int getThirdQuartile() {
+        return thirdQuartile;
+    }
+}
--- StatisticsData.java
+++ StatisticsData.java
@@ -20,7 +20,7 @@ public class StatisticsData extends DataHandler
             "Harrow", "Brent", "Barnet", "Enfield", "Waltham Forest", "Redbridge", "Sutton", "Lambeth", "Southwark",
             "Lewisham", "Greenwich", "Bexley", "Richmond upon Thames", "Merton", "Wandsworth", "Hammersmith and Fulham", 
             "Kensington and Chelsea", "City of London", "Westminster", "Camden", "Tower Hamlets", "Islington", "Hackney",
-            "Haringey", "Newham", "Barking and Dagenham", "test"};
+            "Haringey", "Newham", "Barking and Dagenham"};
 
     /**
      * Constructor for objects of class StatisticsData
@@ -178,74 +178,121 @@ public class StatisticsData extends DataHandler
         return information; 
     }
     
-    public Color getBoroughMapColour(String boroughName, int minPrice, int maxPrice, ArrayList<BoroughListing> sortedNumberOfPropertiesInBorough) {
-        Random rand = new Random();
+    // public Color getBoroughMapColour(String boroughName, int minPrice, int maxPrice, ArrayList<BoroughListing> sortedNumberOfPropertiesInBorough) {
+        // Random rand = new Random();
         
-        //Color boroughColour = Color.rgb(rand.nextInt(256),rand.nextInt(256),rand.nextInt(256));
+        // //Color boroughColour = Color.rgb(rand.nextInt(256),rand.nextInt(256),rand.nextInt(256));
         
-        int noOfBoroughs = sortedNumberOfPropertiesInBorough.size();
+        // int noOfBoroughs = sortedNumberOfPropertiesInBorough.size();
         
-        BoroughListing boroughWithMinNoOfProperties = sortedNumberOfPropertiesInBorough.get(0);
-        BoroughListing boroughWithMedianNoOfProperties;
-        BoroughListing boroughWithMaxNoOfProperties = sortedNumberOfPropertiesInBorough.get(sortedNumberOfPropertiesInBorough.size()-1);
+        // BoroughListing boroughWithMinNoOfProperties = sortedNumberOfPropertiesInBorough.get(0);
+        // BoroughListing boroughWithMedianNoOfProperties;
+        // BoroughListing boroughWithMaxNoOfProperties = sortedNumberOfPropertiesInBorough.get(sortedNumberOfPropertiesInBorough.size()-1);
         
-        int minNoOfPropertiesInBorough = boroughWithMinNoOfProperties.getNoOfPropertiesInBorough();
-        int median;
-        int maxNoOfPropertiesInBorough = boroughWithMaxNoOfProperties.getNoOfPropertiesInBorough();
+        // int minNoOfPropertiesInBorough = boroughWithMinNoOfProperties.getNoOfPropertiesInBorough();
+        // int median;
+        // int maxNoOfPropertiesInBorough = boroughWithMaxNoOfProperties.getNoOfPropertiesInBorough();
         
-        if (noOfBoroughs % 2 == 1) {
-            boroughWithMedianNoOfProperties = sortedNumberOfPropertiesInBorough.get(Math.round(noOfBoroughs/2)-1);
-            median = boroughWithMedianNoOfProperties.getNoOfPropertiesInBorough();
-        }
-        else {
-            BoroughListing boroughLowerOfMedian = sortedNumberOfPropertiesInBorough.get(Math.round(noOfBoroughs/2)-1);
-            BoroughListing boroughUpperOfMedian = sortedNumberOfPropertiesInBorough.get(Math.round(noOfBoroughs/2));
-            
-            int boroughLowerOfMedianSize = boroughLowerOfMedian.getNoOfPropertiesInBorough();
-            int boroughUpperOfMedianSize = boroughUpperOfMedian.getNoOfPropertiesInBorough();
-            
-            median = (int) ((boroughLowerOfMedianSize + boroughUpperOfMedianSize) / 2);
-        }
-        
-        int noOfPropertiesInBorough = getPropertiesFromBorough(boroughName, minPrice, maxPrice).size();
-        
-        // double percentile = (double) (noOfPropertiesInBorough - minNoOfPropertiesInBorough) / (double) (maxNoOfPropertiesInBorough - minNoOfPropertiesInBorough);
-        
-        // if (percentile > 0.0 && percentile < 0.25) {
-            // percentile = 0.0;
-        // }
-        // else if (percentile < 0.50) {
-            // percentile = 0.375;
-        // }
-        // else if (percentile < 0.75) {
-            // percentile = 0.625;
+        // if (noOfBoroughs % 2 == 1) {
+            // boroughWithMedianNoOfProperties = sortedNumberOfPropertiesInBorough.get(Math.round(noOfBoroughs/2)-1);
+            // median = boroughWithMedianNoOfProperties.getNoOfPropertiesInBorough();
         // }
         // else {
-            // percentile = 1.00;
+            // BoroughListing boroughLowerOfMedian = sortedNumberOfPropertiesInBorough.get(Math.round(noOfBoroughs/2)-1);
+            // BoroughListing boroughUpperOfMedian = sortedNumberOfPropertiesInBorough.get(Math.round(noOfBoroughs/2));
+            
+            // int boroughLowerOfMedianSize = boroughLowerOfMedian.getNoOfPropertiesInBorough();
+            // int boroughUpperOfMedianSize = boroughUpperOfMedian.getNoOfPropertiesInBorough();
+            
+            // median = (int) ((boroughLowerOfMedianSize + boroughUpperOfMedianSize) / 2);
         // }
         
-        //Color boroughColour = Color.rgb(0, 255 - (int)(255*percentile), 0);
+        // int noOfPropertiesInBorough = getPropertiesFromBorough(boroughName, minPrice, maxPrice).size();
         
-        double halfOfMedian = median * 0.5;
-        Color boroughColour;
+        // // double percentile = (double) (noOfPropertiesInBorough - minNoOfPropertiesInBorough) / (double) (maxNoOfPropertiesInBorough - minNoOfPropertiesInBorough);
+        
+        // // if (percentile > 0.0 && percentile < 0.25) {
+            // // percentile = 0.0;
+        // // }
+        // // else if (percentile < 0.50) {
+            // // percentile = 0.375;
+        // // }
+        // // else if (percentile < 0.75) {
+            // // percentile = 0.625;
+        // // }
+        // // else {
+            // // percentile = 1.00;
+        // // }
+        
+        // //Color boroughColour = Color.rgb(0, 255 - (int)(255*percentile), 0);
+        
+        // double halfOfMedian = median * 0.5;
+        // Color boroughColour;
+        
+        // if (noOfPropertiesInBorough == 0) {
+            // boroughColour = Color.rgb(255, 255, 255);
+        // }
+        // else if (noOfPropertiesInBorough < median - halfOfMedian) {
+            // boroughColour = Color.rgb(180, 255, 180);
+        // }
+        // else if (noOfPropertiesInBorough < median) {
+            // boroughColour = Color.rgb(80, 235, 80);
+        // }
+        // else if (noOfPropertiesInBorough < median + halfOfMedian) {
+            // boroughColour = Color.rgb(30, 225, 30);
+        // }
+        // else {
+            // boroughColour = Color.rgb(0, 175, 0);
+        // }
+        
+        // return boroughColour;
+    // }
+    
+    public double getBoroughMapColour(String boroughName, int minPrice, int maxPrice, NoOfPropertiesStats noOfPropertiesStats) {     
+        int noOfPropertiesInBorough = getPropertiesFromBorough(boroughName, minPrice, maxPrice).size();
+        
+        double brightness = getBrightness(noOfPropertiesInBorough, noOfPropertiesStats);
+        
+        return brightness;
+    }
+    
+    public double getBoroughMapColour(double percentile, NoOfPropertiesStats noOfPropertiesStats) {
+        int median = noOfPropertiesStats.getMedian();
+        
+        int calculatedNoOfPropertiesInBorough = (int)((median * 2) * percentile);
+        
+        double brightness = getBrightness(calculatedNoOfPropertiesInBorough, noOfPropertiesStats);
+        
+        return brightness;
+    }
+    
+    private double getBrightness(int noOfPropertiesInBorough, NoOfPropertiesStats noOfPropertiesStats) 
+    {
+        int median = noOfPropertiesStats.getMedian();
+        int firstQuartile = noOfPropertiesStats.getFirstQuartile();
+        int thirdQuartile = noOfPropertiesStats.getThirdQuartile();
+        
+        double brightness;
         
         if (noOfPropertiesInBorough == 0) {
-            boroughColour = Color.rgb(255, 255, 255);
+            brightness = 0.00;
         }
-        else if (noOfPropertiesInBorough < median - halfOfMedian) {
-            boroughColour = Color.rgb(180, 255, 180);
+        else if (noOfPropertiesInBorough < firstQuartile) {
+            brightness = 0.1;
         }
         else if (noOfPropertiesInBorough < median) {
-            boroughColour = Color.rgb(80, 235, 80);
+            brightness = 0.4;
         }
-        else if (noOfPropertiesInBorough < median + halfOfMedian) {
-            boroughColour = Color.rgb(30, 225, 30);
+        else if (noOfPropertiesInBorough < thirdQuartile) {
+            brightness = 0.65;
         }
         else {
-            boroughColour = Color.rgb(0, 175, 0);
+            brightness = 0.8;
         }
         
-        return boroughColour;
+        brightness = (brightness-0.5) * -2;
+        
+        return brightness;
     }
     
     public ArrayList<BoroughListing> getSortedNumberOfPropertiesInBoroughs(int minPrice, int maxPrice) throws Exception
