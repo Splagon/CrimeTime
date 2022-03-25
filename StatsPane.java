903ad5440a59713178d7f32ce453354a8e55f21f
K21005255 <K21005255@kcl.ac.uk>
2022-03-25 12:17:18 +0000

    Commenting and code cleanup

--- StatsPane.java
+++ StatsPane.java
@@ -43,15 +43,25 @@ public class StatsPane extends MainViewerPane
         }
         currentStat = 0; 
         
+        
+        // The "title" labels in the window
+        Label title = new Label("General Statistics");
+        Label reviewTitle = new Label("Average Reviews Per Property:");
+        Label availableTitle = new Label("Total Available Properties:");
+        Label noHomeAndApartmentsTitle = new Label("Entire Home and Apartments:");
+        Label expensiveTitle = new Label("Most Expensive Borough:");
+        Label priceSDTitle = new Label("Standard Deviation of Price (£):");
+        Label highAvgReviewTitle = new Label("Borough with the Highest \nAverage Amount of Reviews:"); 
+        
+        // The "info" labels for each statistic which displays the data value
         Label reviewInfo = new Label("default");
         Label availableInfo = new Label("default");
         Label noHomeAndApartmentsInfo = new Label("default");
         Label expensiveInfo = new Label("default");
         Label priceSDInfo = new Label("default");
-        Label highAvgReviewInfo = new Label("default:"); 
-        XYChart.Series averagePriceData = new XYChart.Series();
+        Label highAvgReviewInfo = new Label("default:");
     
-        // The layout of the window
+        // A grid holding some statistics
         GridPane statsGrid = new GridPane(); 
         VBox reviews = new VBox();
         VBox available = new VBox(); 
@@ -61,76 +71,45 @@ public class StatsPane extends MainViewerPane
         VBox averagePrice = new VBox(); 
         VBox highAvgReview = new VBox();
         
-        CategoryAxis xAxis = new CategoryAxis();
-        NumberAxis yAxis = new NumberAxis();
-        BarChart barChart = new BarChart(xAxis, yAxis);
-          
+        // Add Labels to the reviews VBox
+        reviews.setAlignment(Pos.CENTER);
+        reviews.getChildren().add(reviewTitle); 
+        reviews.getChildren().add(reviewInfo);
         
-        // The "title" labels in the window
-        Label title = new Label("General Statistics");
-        Label reviewTitle = new Label("Average Reviews Per Property:");
-        Label availableTitle = new Label("Total Available Properties:");
-        Label noHomeAndApartmentsTitle = new Label("Entire Home and Apartments:");
-        Label expensiveTitle = new Label("Most Expensive Borough:");
-        Label priceSDTitle = new Label("Standard Deviation of Price (£):");
-        Label highAvgReviewTitle = new Label("Borough with the Highest \nAverage Amount of Reviews:");
+        // Add Labels to the available VBox
+        available.setAlignment(Pos.CENTER);
+        available.getChildren().add(availableTitle); 
+        available.getChildren().add(availableInfo);
         
-        VBox statsGridVBox = new VBox();
-        statsGridVBox.getChildren().add(statsGrid);
+        // Add Labels to the noHomeAndApartments VBox
+        noHomeAndApartments.setAlignment(Pos.CENTER);
+        noHomeAndApartments.getChildren().add(noHomeAndApartmentsTitle); 
+        noHomeAndApartments.getChildren().add(noHomeAndApartmentsInfo);
         
-        VBox AvgPriceBarCharVBox = new VBox();
-        AvgPriceBarCharVBox.getChildren().add(createBarChart(getAveragePricePerBoroughData()));
+        // Add Labels to the expensive VBox
+        expensive.setAlignment(Pos.CENTER);
+        expensive.getChildren().add(expensiveTitle); 
+        expensive.getChildren().add(expensiveInfo);
         
-        VBox AvgReviewsBarCharVBox = new VBox();
-        AvgReviewsBarCharVBox.getChildren().add(createBarChart(getAverageReviewsPerBoroughData()));
-    
-        statsOrder.add(statsGridVBox);
-        statsOrder.add(AvgPriceBarCharVBox);
-        statsOrder.add(AvgReviewsBarCharVBox);
-        // Adding components 
-        statsWindow.setAlignment(Pos.CENTER);
-        statsWindow.getChildren().add(title); 
-        statsWindow.getChildren().add(getStatsIndex(0)); 
-        //window.getChildren().add(barChart);
-        title.setAlignment(Pos.CENTER);
-        statsGrid.setAlignment(Pos.CENTER); 
+        // Add Labels to the priceSD VBox
+        priceSD.setAlignment(Pos.CENTER);
+        priceSD.getChildren().add(priceSDTitle); 
+        priceSD.getChildren().add(priceSDInfo);
+        
+        // Add Labels to the highAvgReview VBox
+        highAvgReview.setAlignment(Pos.CENTER);
+        highAvgReview.getChildren().add(highAvgReviewTitle); 
+        highAvgReview.getChildren().add(highAvgReviewInfo);
         
         statsGrid.add(reviews, 0, 0);
         statsGrid.add(available, 0, 1);
         statsGrid.add(noHomeAndApartments, 1, 0);
         statsGrid.add(expensive, 1, 1);
         statsGrid.add(priceSD, 0, 2); 
-        statsGrid.add(highAvgReview, 1 , 2); 
+        statsGrid.add(highAvgReview, 1 , 2);
         
-        reviews.setAlignment(Pos.CENTER);
-        reviews.getChildren().add(reviewTitle); 
-        reviews.getChildren().add(reviewInfo);
         
-        available.setAlignment(Pos.CENTER);
-        available.getChildren().add(availableTitle); 
-        available.getChildren().add(availableInfo);
-        
-        noHomeAndApartments.setAlignment(Pos.CENTER);
-        noHomeAndApartments.getChildren().add(noHomeAndApartmentsTitle); 
-        noHomeAndApartments.getChildren().add(noHomeAndApartmentsInfo);
-        
-        expensive.setAlignment(Pos.CENTER);
-        expensive.getChildren().add(expensiveTitle); 
-        expensive.getChildren().add(expensiveInfo);
-        
-        priceSD.setAlignment(Pos.CENTER);
-        priceSD.getChildren().add(priceSDTitle); 
-        priceSD.getChildren().add(priceSDInfo);
-        
-        highAvgReview.setAlignment(Pos.CENTER);
-        highAvgReview.getChildren().add(highAvgReviewTitle); 
-        highAvgReview.getChildren().add(highAvgReviewInfo);
-        
-        //Set the scene and add CSS
-        //Scene scene = new Scene(window, 1200,700);
-        
-        //scene.getStylesheets().add("stylesheet.css");
-        
+        statsGrid.setAlignment(Pos.CENTER);
         statsGrid.setId("statsgrid"); 
         statsWindow.getStyleClass().add("statsWindowAndButtons");
         reviews.getStyleClass().add("statsvbox"); 
@@ -148,7 +127,44 @@ public class StatsPane extends MainViewerPane
         priceSDInfo.getStyleClass().add("statslabels"); 
         highAvgReviewInfo.getStyleClass().add("statslabels");
         
-        title.getStyleClass().add("windowTitle"); 
+        
+        
+        
+        
+        
+        
+        
+        
+        VBox statsGridVBox = new VBox();
+        statsGridVBox.getChildren().add(statsGrid);
+        
+        VBox AvgPriceBarChartVBox = new VBox();
+        BarChart AvgPriceBarChart = createBarChart(getBarChartData(StatisticsData.getAveragePricePerBorough()), "Boroughs", "Average Price", "Average Price per Night per Borough");
+        AvgPriceBarChartVBox.getChildren().add(AvgPriceBarChart);
+        
+        VBox AvgReviewsBarChartVBox = new VBox();
+        BarChart AvgReviewsBarChart = createBarChart(getBarChartData(StatisticsData.getAverageReviewsPerBorough()), "Boroughs", "Average Reviews", "Average Number of Reviews per Borough");
+        AvgReviewsBarChartVBox.getChildren().add(AvgReviewsBarChart);
+    
+        statsOrder.add(statsGridVBox);
+        statsOrder.add(AvgPriceBarChartVBox);
+        statsOrder.add(AvgReviewsBarChartVBox);
+        // Adding components 
+        statsWindow.setAlignment(Pos.CENTER);
+        statsWindow.getChildren().add(title); 
+        statsWindow.getChildren().add(getStatsOrderObject(0)); 
+        title.setAlignment(Pos.CENTER); 
+        title.getStyleClass().add("windowTitle");
+        
+        
+        
+        //Set the scene and add CSS
+        //Scene scene = new Scene(window, 1200,700);
+        
+        //scene.getStylesheets().add("stylesheet.css");
+    
+        
+         
         
     
         setText(reviewInfo, StatisticsData.getAverageNoReviews(false));
@@ -192,22 +208,31 @@ public class StatsPane extends MainViewerPane
         statsPane = statsBorder;
     }
     
-    private BarChart createBarChart(XYChart.Series data)
+    /**
+     * Create a Bar Chart holding data 
+     * @params XYChart.Series chart - The data to go into the chart. String xAxisLabel - Label of the xAxis; String yAxisLabel - Label of the yAxis; String dataName - The label of the data.
+     * @return A BarChart object
+     */
+    private BarChart createBarChart(XYChart.Series chart, String xAxisLabel, String yAxisLabel, String dataName)
     {
-        XYChart.Series averagePriceData = new XYChart.Series();
+        XYChart.Series data = new XYChart.Series();
         CategoryAxis xAxis = new CategoryAxis();
         NumberAxis yAxis = new NumberAxis();
         BarChart barChart = new BarChart(xAxis, yAxis);
-        xAxis.setLabel("Borough");
-        yAxis.setLabel("Average Price");
-        averagePriceData = data;
-        averagePriceData.setName("Average Price per Night per Borough");
-        barChart.getData().add(averagePriceData);
-        
+        xAxis.setLabel(xAxisLabel);
+        yAxis.setLabel(yAxisLabel);
+        data = chart;
+        data.setName(dataName);
+        barChart.getData().add(data);
         return barChart;
     }
     
-    private VBox getStatsIndex(int x)
+    /**
+     * Get a VBox object from the Stats Order List at a certain index. Index must be within the List's bounds. 
+     * @params int x - Index of the object needed.
+     * @return VBox Object from the Stats Order List.
+     */
+    private VBox getStatsOrderObject(int x)
     {
         if(statsOrder.size() > 0 && x >= 0 && x < statsOrder.size())
         {
@@ -218,11 +243,10 @@ public class StatsPane extends MainViewerPane
     }
     
     /**
-     * Action for the left button which will go the element at index current - 1 in both
-     * title list and stat list. If it reaches the start of both of the lists, set the labels to
-     * the last element in the lists. 
+     * Action for the left button which will go the element at index current - 1. If the first element is reached,
+     * it will set the window content to the last element in the Stats Order list.  
      */
-    public void leftStatButtonAction()
+    private void leftStatButtonAction()
     {
         if(currentStat - 1 < 0)
         {
@@ -235,11 +259,10 @@ public class StatsPane extends MainViewerPane
     }
     
     /**
-     * Action for the right button which will go the element at index current + 1 in both
-     * title list and stat list. If it reaches the end of both of the lists, set the labels to
-     * the first element in the lists.
+     * Action for the right button which will go the element at index current + 1. If the last element is reached,
+     * it will set the window content to the first element in the Stats Order list.  
      */
-    public void rightStatButtonAction()
+    private void rightStatButtonAction()
     {
         if(currentStat + 1 >= statsOrder.size())
         {
@@ -251,15 +274,23 @@ public class StatsPane extends MainViewerPane
         }
     }
     
+    /**
+     * Used to swap window content for the different Statistics displays (e.g. grid, barchat). 
+     * @params int change - set the current index of Stats Order list to this int. 
+     */
     private void swapWindowContent(int change)
     {
         //fadeOut(statsWindow, 1000);
-        statsWindow.getChildren().remove(statsOrder.get(currentStat));
+        statsWindow.getChildren().remove(getStatsOrderObject(currentStat));
         currentStat = change; 
-        statsWindow.getChildren().add(statsOrder.get(currentStat));
+        statsWindow.getChildren().add(getStatsOrderObject(currentStat));
         fadeIn(statsWindow, 1000);
     }
     
+    /**
+     * Fade a certain node out
+     * @params Node node - The node in which this will happen to; long time - The amount of time that the animation will last for. 
+     */
     private void fadeOut(Node node, long time)
     {
         FadeTransition effect = new FadeTransition(Duration.millis(time), node);
@@ -268,6 +299,10 @@ public class StatsPane extends MainViewerPane
         effect.play();
     }
     
+    /**
+     * Fade a certain node in
+     * @params Node node - The node in which this will happen to; long time - The amount of time that the animation will last for. 
+     */
     private void fadeIn(Node node, long time)
     {
         FadeTransition effect = new FadeTransition(Duration.millis(time), node);
@@ -276,44 +311,48 @@ public class StatsPane extends MainViewerPane
         effect.play();
     }
     
+    /**
+     * Convert a double data type to String then set it as a Label's text
+     * @params Label label - the label which will be affected; double dataToFormat - The data to format into a String
+     */
     private void setText(Label label, double dataToFormat) {
         String formattedLabel = String.valueOf(String.format("%.2f", dataToFormat) + " (2 d.p)"); 
         label.setText(formattedLabel);
     }
     
+    /**
+     * Convert an int data type to String then set it as a Label's text
+     * @params Label label - the label which will be affected; int dataToFormat - The data to format into a String
+     */
     private void setText(Label label, int dataToFormat) {
         label.setText(String.valueOf(dataToFormat));
     }
     
+    /**
+     * Set a Label's text to a passed through String
+     * @params Label label - the label which will be affected; String dataToFormat - The String that the Label's text will be set to
+     */
     private void setText(Label label, String dataToFormat) {
         label.setText(dataToFormat);
     }
     
     /**
-     * Hola Matthew! this should probably one method with a variable. Gracias
+     * @params HashMap<String, Integer> map - Containing data for each borough
+     * @return XYChart.Series object
      */
-    private XYChart.Series getAveragePricePerBoroughData()
+    private XYChart.Series getBarChartData(HashMap<String, Integer> map)
     {
         XYChart.Series data = new XYChart.Series();
-        HashMap<String, Integer> information = StatisticsData.getAveragePricePerBorough();
-        for (String entry : information.keySet())
+        for (String entry : map.keySet())
         {
-            data.getData().add(new XYChart.Data(entry, information.get(entry)));
+            data.getData().add(new XYChart.Data(entry, map.get(entry)));
         }
         return data; 
     }
     
-    private XYChart.Series getAverageReviewsPerBoroughData()
-    {
-        XYChart.Series data = new XYChart.Series();
-        HashMap<String, Integer> information = StatisticsData.getAverageReviewsPerBorough();
-        for (String entry : information.keySet())
-        {
-            data.getData().add(new XYChart.Data(entry, information.get(entry)));
-        }
-        return data; 
-    }
-    
+    /**
+     * @return The statsPane as a Pane object
+     */
     public Pane getPane() {
         return statsPane;
     }
